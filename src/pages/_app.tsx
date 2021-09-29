cc789d75d309c4e21be0969dafd6290fa63ef7e4
gari8 <hagari.hayato.infratop@gmail.com>
2021-09-29 13:11:39 +0900

    install chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {},
+};
+
+const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
