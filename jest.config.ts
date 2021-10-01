ce70cbe3ba81c6e0cbca8f0da6776070dd64e2c7
gari8 <hagari.hayato.infratop@gmail.com>
2021-10-01 16:52:31 +0900

    hotfix/mock

--- jest.config.ts
+++ jest.config.ts
@@ -17,6 +17,8 @@ export default async (): Promise<Config.InitialOptions> => {
       "!**/node_modules/**",
     ],
     moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "<rootDir>/__mocks__/fileMock.js",
       "\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
       "^@/components/(.*)$": "<rootDir>/components/$1",
       "^@/pages/(.*)$": "<rootDir>/pages/$1",
